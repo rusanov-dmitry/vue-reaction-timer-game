f8eea3cacc4d76685ab72720f9b76e0f9e0dc04d
rusanov-dmitry <r0llit@protonmail.com>
2022-02-10 14:32:12 +0600

    build reaction timer game

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <button @click="startGame" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score">
+    <p>Your score is: <span>{{ score }} ms</span></p>
+  </Results>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    startGame() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
   }
 }
 </script>
@@ -20,7 +43,20 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+button {
+  padding: 15px 35px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  font-weight: 600;
+  background: rgb(120, 83, 207);
+  color: white;
+  cursor: pointer;
+}
+button:disabled {
+  background: rgb(187, 168, 231);
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="score < 300" class="result">
+    <h2>Supa fast!</h2>
+    <slot></slot>
+  </div>
+  <div v-else-if="score < 400" class="result">
+    <h2>Okayge</h2>
+    <slot></slot>
+  </div>
+  <div v-else class="result">
+    <h2>Supa slow...</h2>
+    <slot></slot>
+  </div>
+  <p>Click play to test your reaction again!</p>
+</template>
+
+<script>
+export default {
+  props: ['score']
+}
+</script>
+
+<style>
+.result {
+  margin-top: 60px;
+}
+.result h2 {
+  color: #0faf87;
+}
+.result p {
+  font-size: 18px;
+}
+.result p span {
+  font-weight: 800;
+}
+</style>
